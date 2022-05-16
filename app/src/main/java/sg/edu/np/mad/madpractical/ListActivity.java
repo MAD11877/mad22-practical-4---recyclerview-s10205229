b3e599523652ac5af3d04e71d845aa5122f4983b
Kai Ling Chua <s10205229@connect.np.edu.sg>
2022-05-16 15:38:34 +0800

    done 2

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/ListActivity.java
@@ -0,0 +1,59 @@
+package sg.edu.np.mad.madpractical;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        ArrayList<User> userList = GenerateUsers(200);
+        InitRecycler(userList);
+    }
+
+    public ArrayList<User> GenerateUsers(int noUsers){
+        ArrayList<User> userList = new ArrayList<>();
+
+        for(int userID = 0; userID < noUsers; userID++){
+            String username = "Name" + Integer.toString(new Random().nextInt(9999999));
+            String desc = "Description" + Integer.toString(new Random().nextInt(9999999));
+            // String desc = String.format("Description %d", new Random().nextInt(9999999));
+            boolean followed = new Random().nextBoolean();
+
+            User user = new User(username, desc, userID, followed);
+
+            userList.add(user);
+        }
+
+        return userList;
+    }
+
+    public void InitRecycler(ArrayList<User> userList) {
+        RecyclerView recyclerView = findViewById(R.id.RecyclerView);
+
+        BrandsAdapter mAdapter =  new BrandsAdapter(userList);
+
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+    }
+
+
+}
